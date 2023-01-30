5b5ecf3c544b4e1fd1da4fa1a294e7073557350f
zonkiie <33644484+zonkiie@users.noreply.github.com>
2023-01-30 15:27:20 +0100

    working

--- ldap2_fdw.c
+++ ldap2_fdw.c
@@ -1,5 +1,6 @@
 /*
  * Include all neccessary headers
+ * The frame is form fdw_dummy.c
  */
 
 #include "postgres.h"
@@ -27,4 +28,150 @@ PG_FUNCTION_INFO_V1(ldap2_fdw_handler);
 
 void		_PG_init(void);
 void		_PG_fini(void);
+
+static void ldap2_fdw_GetForeignRelSize(PlannerInfo *root,
+						   RelOptInfo *baserel,
+						   Oid foreigntableid);
+static void ldap2_fdw_GetForeignPaths(PlannerInfo *root,
+						 RelOptInfo *baserel,
+						 Oid foreigntableid);
+#if (PG_VERSION_NUM <= 90500)
+static ForeignScan *ldap2_fdw_GetForeignPlan(PlannerInfo *root,
+						RelOptInfo *baserel,
+						Oid foreigntableid,
+						ForeignPath *best_path,
+						List *tlist,
+						List *scan_clauses);
+
+#else
+static ForeignScan *ldap2_fdw_GetForeignPlan(PlannerInfo *root,
+						RelOptInfo *baserel,
+						Oid foreigntableid,
+						ForeignPath *best_path,
+						List *tlist,
+						List *scan_clauses,
+						Plan *outer_plan);
+#endif
+static void ldap2_fdw_BeginForeignScan(ForeignScanState *node, int eflags);
+static TupleTableSlot *ldap2_fdw_IterateForeignScan(ForeignScanState *node);
+static void ldap2_fdw_ReScanForeignScan(ForeignScanState *node);
+static void ldap2_fdw_EndForeignScan(ForeignScanState *node);
+
+
+/*
+ * FDW callback routines
+ */
+static void ldap2_fdw_AddForeignUpdateTargets(Query *parsetree,
+								RangeTblEntry *target_rte,
+								Relation target_relation);
+static List *ldap2_fdw_PlanForeignModify(PlannerInfo *root,
+						  ModifyTable *plan,
+						  Index resultRelation,
+						  int subplan_index);
+static void ldap2_fdw_BeginForeignModify(ModifyTableState *mtstate,
+						   ResultRelInfo *resultRelInfo,
+						   List *fdw_private,
+						   int subplan_index,
+						   int eflags);
+static TupleTableSlot *ldap2_fdw_ExecForeignInsert(EState *estate,
+						  ResultRelInfo *resultRelInfo,
+						  TupleTableSlot *slot,
+						  TupleTableSlot *planSlot);
+static TupleTableSlot *ldap2_fdw_ExecForeignUpdate(EState *estate,
+						  ResultRelInfo *resultRelInfo,
+						  TupleTableSlot *slot,
+						  TupleTableSlot *planSlot);
+static TupleTableSlot *ldap2_fdw_ExecForeignDelete(EState *estate,
+						  ResultRelInfo *resultRelInfo,
+						  TupleTableSlot *slot,
+						  TupleTableSlot *planSlot);
+static void ldap2_fdw_EndForeignModify(EState *estate,
+						 ResultRelInfo *resultRelInfo);
+static int	ldap2_fdw_IsForeignRelUpdatable(Relation rel);
+static void ldap2_fdw_ExplainForeignScan(ForeignScanState *node,
+						   ExplainState *es);
+static void ldap2_fdw_ExplainForeignModify(ModifyTableState *mtstate,
+							 ResultRelInfo *rinfo,
+							 List *fdw_private,
+							 int subplan_index,
+							 ExplainState *es);
+static bool ldap2_fdw_AnalyzeForeignTable(Relation relation,
+							AcquireSampleRowsFunc *func,
+							BlockNumber *totalpages);
+static int ldap2_fdw_AcquireSampleRowsFunc(Relation relation, int elevel,
+							  HeapTuple *rows, int targrows,
+							  double *totalrows,
+							  double *totaldeadrows);
+
+
+
+/* magic */
+enum FdwScanPrivateIndex
+{
+    /* SQL statement to execute remotely (as a String node) */
+    FdwScanPrivateSelectSql,
+    /* Integer list of attribute numbers retrieved by the SELECT */
+    FdwScanPrivateRetrievedAttrs
+};
+/*
+ * Similarly, this enum describes what's kept in the fdw_private list for
+ * a ModifyTable node referencing a postgres_fdw foreign table.  We store:
+ *
+ * 1) INSERT/UPDATE/DELETE statement text to be sent to the remote server
+ * 2) Integer list of target attribute numbers for INSERT/UPDATE
+ *    (NIL for a DELETE)
+ * 3) Boolean flag showing if there's a RETURNING clause
+ * 4) Integer list of attribute numbers retrieved by RETURNING, if any
+ */
+enum FdwModifyPrivateIndex
+{
+    /* SQL statement to execute remotely (as a String node) */
+    FdwModifyPrivateUpdateSql,
+    /* Integer list of target attribute numbers for INSERT/UPDATE */
+    FdwModifyPrivateTargetAttnums,
+    /* has-returning flag (as an integer Value node) */
+    FdwModifyPrivateHasReturning,
+    /* Integer list of attribute numbers retrieved by RETURNING */
+    FdwModifyPrivateRetrievedAttrs
+};
+
+
+void _PG_init() 
+{
+}
+
+void _PG_fini()
+{
+}
+
+Datum ldap2_fdw_handler(PG_FUNCTION_ARGS)
+{
+    FdwRoutine *fdw_routine = makeNode(FdwRoutine);
+
+    fdw_routine->GetForeignRelSize = ldap2_fdw_GetForeignRelSize;
+    fdw_routine->GetForeignPaths = ldap2_fdw_GetForeignPaths;
+    fdw_routine->GetForeignPlan = ldap2_fdw_GetForeignPlan;
+    fdw_routine->ExplainForeignScan = ldap2_fdw_ExplainForeignScan;
+    fdw_routine->ExplainForeignModify = ldap2_fdw_ExplainForeignModify;
+
+    fdw_routine->BeginForeignScan = ldap2_fdw_BeginForeignScan;
+    fdw_routine->IterateForeignScan = ldap2_fdw_IterateForeignScan;
+    fdw_routine->ReScanForeignScan = ldap2_fdw_ReScanForeignScan;
+    fdw_routine->EndForeignScan = ldap2_fdw_EndForeignScan;
+
+    /* insert support */
+    fdw_routine->AddForeignUpdateTargets = ldap2_fdw_AddForeignUpdateTargets;
+
+    fdw_routine->PlanForeignModify = ldap2_fdw_PlanForeignModify;
+    fdw_routine->BeginForeignModify = ldap2_fdw_BeginForeignModify;
+    fdw_routine->ExecForeignInsert = ldap2_fdw_ExecForeignInsert;
+    fdw_routine->ExecForeignUpdate = ldap2_fdw_ExecForeignUpdate;
+    fdw_routine->ExecForeignDelete = ldap2_fdw_ExecForeignDelete;
+    fdw_routine->EndForeignModify = ldap2_fdw_EndForeignModify;
+    fdw_routine->IsForeignRelUpdatable = ldap2_fdw_IsForeignRelUpdatable;
+
+    fdw_routine->AnalyzeForeignTable = ldap2_fdw_AnalyzeForeignTable;
+
+    PG_RETURN_POINTER(fdw_routine);
+}
 
