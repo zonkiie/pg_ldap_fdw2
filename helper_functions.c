4df981f00eb7cdebf43a966b43a94042a1761e52
zonkiie <33644484+zonkiie@users.noreply.github.com>
2023-01-31 17:16:35 +0100

    working

--- helper_functions.c
+++ helper_functions.c
@@ -1,5 +1,7 @@
 include "helper_functions.h"
 
+struct         timeval  zerotime = {.tv_sec = 0L, .tv_usec = 0L};
+
 int str_split(char ***dest, char *str, char *separator)
 {
 	int el_count = substr_count(str, separator) + 1;
@@ -54,4 +56,85 @@ int substr_count(char *str, char *substr)
 	return count;
 }
 
+void free_cstr(char ** str)
+{
+	if(*str == NULL) return;
+	free(*str);
+	*str = NULL;
+}
+
+void reassign_cstr(char **str, const char * value)
+{
+	free_cstr(str);
+	*str = strdup(value);
+}
+
+int get_carr_size(char ** carr)
+{
+	if(carr == NULL) return 0;
+    int i = 0;
+    for(; carr[i] != NULL; i++);
+    return i;
+}
+
+/** Free c array strings and null memory */
+void free_carr_n(char ***carr)
+{
+    if(carr == NULL || *carr == NULL) return;
+	int size = get_carr_size(*carr);
+	for(int i = size - 1; i >= 0; i--) 
+	{
+		free((*carr)[i]);
+		(*carr)[i] = NULL;
+	}
+	free(*carr);
+	*carr = NULL;
+}
+
+void free_file(FILE** file)
+{
+	if(*file == NULL) return;
+	fflush(*file);
+	fclose(*file);
+	*file = NULL;
+}
+
+char * quote_string(const char *str, quote_strings * quot)
+{
+	_cleanup_cstr_ char * quoted_array_delimiter;
+	_cleanup_cstr_ char * quoted_attribute_delimiter;
+	asprintf(&quoted_array_delimiter, "\\%s", quot->array_delimiter);
+	asprintf(&quoted_attribute_delimiter, "\\%s", quot->attribute_delimiter);
+	
+	_cleanup_carr_ char ** step = (char**)calloc(10, sizeof(char*));
+	step[0] = str_replace(str, quot->array_delimiter, quoted_array_delimiter);
+	step[1] = str_replace(step[0], "\"", "\"\"\"\"");
+	step[2] = str_replace(step[1], "\n", "\\n");
+	step[3] = str_replace(step[2], "\r", "\\r");
+	step[4] = str_replace(step[3], quot->attribute_delimiter, quoted_attribute_delimiter);
+	return strdup(step[4]);
+}
+
+bool in_array(char ** array, char * value)
+{
+	for(int i = 0; array[i] != NULL; i++)
+	{
+		if(!strcmp(array[i], value)) return true;
+	}
+	return false;
+}
+
+/// returns true if value is added, false if value is already in array
+/// array must be big enough that one further value can be added.
+/// value is copied to the new position.
+bool add_to_unique_array(char *** array, char * value)
+{
+	int i = 0;
+	for(; (*array)[i] != NULL; i++)
+	{
+		if(!strcmp((*array)[i], value)) return false;
+	}
+	(*array)[i] = strdup(value);
+	return true;
+}
 
--- helper_functions.h
+++ helper_functions.h
@@ -1,4 +1,12 @@
 #ifndef __helper_functions__
 #define __helper_functions__
 
+#define _cleanup_cstr_ __attribute((cleanup(free_cstr)))
+#define _cleanup_ldap_ __attribute((cleanup(free_ldap)))
+#define _cleanup_ldap_message_ __attribute((cleanup(free_ldap_message)))
+#define _cleanup_ldap_ber_ __attribute((cleanup(free_ber)))
+#define _cleanup_file_ __attribute((cleanup(free_file)))
+#define _cleanup_carr_ __attribute((cleanup(free_carr_n)))
+#define _cleanup_berval_ __attribute((cleanup(free_berval)))
+
 #endif
