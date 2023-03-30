c22fc0f8f791cf0c01cb41b14992ac8a3be1896d
zonkiie <33644484+zonkiie@users.noreply.github.com>
2023-03-30 04:12:09 +0200

    fixed varname

--- ldap_functions.c
+++ ldap_functions.c
@@ -1,10 +1,10 @@
 #include <ldap_functions.h>
 
-int common_ldap_bind(LDAP *ld, const char *username, const char *passwd)
+int common_ldap_bind(LDAP *ld, const char *username, const char *password)
 {
 	_cleanup_berval_ struct berval *berval_password = NULL;
 	if(password != NULL) berval_password = ber_bvstrdup(password);
-	if(!use_sasl) return ldap_simple_bind_s( ld, username, password );
+	if(!use_sasl) return ldap_simple_bind_s( ld, username, password);
 	else if(use_sasl) return ldap_sasl_bind_s( ld, username, LDAP_SASL_SIMPLE, berval_password , NULL, NULL, NULL);
 }
 
